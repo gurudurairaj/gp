e0c14a5dde7b2e139a6627dd3906dea0b97db866
gurudurairaj <46440938+gurudurairaj@users.noreply.github.com>
2019-01-07 10:24:13 +0000

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,15 @@
+f=input()
+if f.isalpha():
+    print("Alphabet")
+elif f.isdigit():
+    print("No")
+else:
+    print("No")
+    f=input()
+if f.isalpha():
+    print("Alphabet")
+elif f.isdigit():
+    print("No")
+else:
+    print("No")
+    
