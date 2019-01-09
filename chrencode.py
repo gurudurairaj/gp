4ed8da0c1608b934c5317c920a10fb1f9e9e3787
gurudurairaj <46440938+gurudurairaj@users.noreply.github.com>
2019-01-09 19:03:31 +0530

    Create chrencode.py

---
+++ chrencode.py
@@ -0,0 +1,20 @@
+s=input()
+s=list(s)
+a=[]
+for i in s:
+    if i=="x":
+        a.append("a")
+    elif i=="y":
+        a.append("b")
+    elif i=="z":
+        a.append("c")
+    elif i=="X":
+        a.append("A")
+    elif i=="Y":
+        a.append("B")
+    elif i=="Z":
+        a.append("C")
+    else:
+        c=ord(i)
+        a.append(chr(c+3))
+print("".join(a))
