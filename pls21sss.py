312b91f4604322752f8209900099101dc18c1b1e
gurudurairaj <46440938+gurudurairaj@users.noreply.github.com>
2019-01-13 23:51:58 +0530

    Update and rename pls21s.py to pls21sss.py

---
+++ pls21sss.py
@@ -0,0 +1,12 @@
+aa,bb=input().split()
+c,d=input().split()
+ee,f=input().split()
+if aa==e==c:
+    print("yes")
+elif bb==d==f:
+    print("yes")
+elif aa==b and c==d and f==ee:
+    print("yes")
+else:
+    print("no")
+    
