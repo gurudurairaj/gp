0ce064733552f071c4c9e96817147f5db1bea3e2
gurudurairaj <46440938+gurudurairaj@users.noreply.github.com>
2019-01-26 16:45:44 +0530

    Create appendzerolas.py

---
+++ appendzerolas.py
@@ -0,0 +1,17 @@
+n=int(input())
+l=list(map(int,input().split()))
+i=0
+c=0
+m=[]
+while i <n:
+    print(i)
+    if l[i]==0:
+        del l[i]
+        n=n-1
+        c=c+1
+        i=i-1
+    i=i+1
+i=0
+for i in range(c):
+    m.append(0)
+print(*(l+m))
