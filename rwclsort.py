cbd77ee6ba65e9dc04a45ade62143aea56cb9f20
gurudurairaj <46440938+gurudurairaj@users.noreply.github.com>
2019-01-29 18:40:13 +0530

    Create rwclsort.py

---
+++ rwclsort.py
@@ -0,0 +1,23 @@
+n,m=map(int,input().split())
+c=min(n,m)
+d=max(n,m)
+l=[]
+h=[]
+for i in range(c):
+    a=list(map(int,input().split()))
+    a.sort()
+    l.append(a)
+for i in range(d):
+    for j in range(c):
+        h.append(l[j][i])
+    h.sort()
+    for k in range(c):
+        l[k][i]=h[k]
+    del h[0:len(h)]
+for i in range(c):
+    for j in range(d):
+        print(l[i][j],end=" ")
+    print()
+
+        
+
