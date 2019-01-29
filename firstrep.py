29522912e186f49a438a0eb5eb4dce360fa7d3cf
gurudurairaj <46440938+gurudurairaj@users.noreply.github.com>
2019-01-29 22:07:20 +0530

    Update and rename frstrep.py to firstrep.py

--- firstrep.py
+++ firstrep.py
@@ -5,6 +5,9 @@ for i in range(n):
     for j in range(i+1,n):
         if a[i]==a[j]:
             g.append(a[i])
-print(g[0])
+if len(g)==0:
+    print("unique")
+else:
+    print(g[0])
             
         
