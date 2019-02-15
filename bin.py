57872162b00c98ffa369a6d1f77b27a1227451c9
gurudurairaj <46440938+gurudurairaj@users.noreply.github.com>
2019-02-15 22:40:45 +0530

    Create bin.py

---
+++ bin.py
@@ -0,0 +1,19 @@
+a=int(input())
+g=""
+l=[]
+for i in range(2**a):
+    x=bin(i).replace("0b","")
+    if len(x)<a:
+        z=str(x)
+        d=a-len(x)
+        for i in range(d):
+            g=g+"0"
+        g=g+x
+        l.append(g)
+        g=""
+    else:
+        l.append(x)
+for i in l:
+    print(i)
+        
+            
