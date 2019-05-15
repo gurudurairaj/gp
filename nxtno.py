3572ff38b6e97a5e800aa6d223180864131ef5bf
gurudurairaj <46440938+gurudurairaj@users.noreply.github.com>
2019-05-15 11:58:30 +0530

    Create nxtno.py

---
+++ nxtno.py
@@ -0,0 +1,17 @@
+a=int(input())
+v=a
+g=str(a)
+g=list(g)
+g.sort()
+c=0
+for i in range(100):
+    v=v+1
+    y=str(v)
+    y=list(y)
+    y.sort()
+    if y==g:
+        c=c+1
+        print(v)
+        break
+if c==0:
+    print("impossible")
