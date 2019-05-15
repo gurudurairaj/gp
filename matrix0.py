590dc9bc970b82b144d225c1a31aa568857e0d1a
gurudurairaj <46440938+gurudurairaj@users.noreply.github.com>
2019-05-15 09:28:52 +0530

    Update and rename mat0.py to matrix0.py

--- matrix0.py
+++ matrix0.py
@@ -3,7 +3,6 @@ a=[]
 b=[]
 for i in range(n):
     a.append(list(map(int,input().split())))
-print(a)
 for i in range(n):
     for j in range(m):
         if a[i][j]==0:
@@ -11,8 +10,9 @@ for i in range(n):
             b.append(j)
 for i in range(0,len(b),2):
     for h in range(m):
-        a[i][h]=0
+        a[b[i]][h]=0
     for h in range(n):
-        a[h][i+1]=0
+        a[h][b[i+1]]=0
 for i in range(n):
     print(*a[i])
+
