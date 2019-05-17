171586dcc3ca7dc55e6c626c8fa574314a82aa8b
gurudurairaj <46440938+gurudurairaj@users.noreply.github.com>
2019-05-17 11:10:51 +0530

    Create palincount.py

---
+++ palincount.py
@@ -0,0 +1,21 @@
+d=input()
+i=0
+kk=1
+j=kk
+c=0
+g=[]
+while i<len(d):
+    c=0
+    while j<=len(d):
+        a=d[c:j]
+        if a not in g:
+            if a==a[::-1] and len(a)>1:
+                g.append(a)
+        j=j+1
+        c=c+1
+    kk=kk+1
+    j=kk
+    i=i+1
+for i in range(len(g)):
+    print(g[i])
+
