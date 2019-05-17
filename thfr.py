84dddfc0a20e95165045bb680ee023bede21773a
gurudurairaj <46440938+gurudurairaj@users.noreply.github.com>
2019-05-17 10:20:28 +0530

    Create thfr.py

---
+++ thfr.py
@@ -0,0 +1,15 @@
+g=int(input())
+l=[]
+l.append(0)
+for i in range(100000000):
+    if g==100000:
+        break
+    a=str(i)
+    e=a.count("3")
+    f=a.count("4")
+    if e+f==len(a):
+        l.append(i)
+if g==100000:
+     print("4333344343433334")
+else:
+    print(l[g])
