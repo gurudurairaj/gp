0b15dc148ae1a2bd4bea3f05f20be9444c8f6da5
gurudurairaj <46440938+gurudurairaj@users.noreply.github.com>
2019-05-21 12:41:13 +0530

    Create k.py

---
+++ k.py
@@ -0,0 +1,10 @@
+n=int(input())
+z=list(map(int,input().split()))
+c=0
+for i in range(len(z)):
+    for j in range(i+1,len(z)):
+        for k in range(j+1,len(z)):
+            if z[i]<z[j]<z[k] and i<j<k:
+                c=c+1
+                print(z[i],z[j],z[k])
+print(c)
