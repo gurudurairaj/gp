02ea4410edd9223503c8eba1e843c10f8dd4fece
gurudurairaj <46440938+gurudurairaj@users.noreply.github.com>
2019-05-21 19:35:01 +0530

    Update lexstr.py

--- lexstr.py
+++ lexstr.py
@@ -1,6 +1,17 @@
 a=input()
 x=list(a)
 x.sort(reverse=True)
-s=x[0]
-f=a.index(s)
-print(a[f:len(a)])
+s=a[0]
+u=0
+ii=""
+for i in range(len(x)):
+    if x[i]==s:
+        break
+    ss=x[i]
+    f=a.index(ss)
+    ss=a[f:len(a)]
+    if len(ss)>u:
+        u=len(ss)
+        ii=ss
+print(ii)
+    
