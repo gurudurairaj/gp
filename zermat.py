53852e88c3da38c5726a8725cdfd2127885a4202
gurudurairaj <46440938+gurudurairaj@users.noreply.github.com>
2019-05-22 12:11:07 +0530

    Create zermat.py

---
+++ zermat.py
@@ -0,0 +1,11 @@
+n=int(input())
+m=list(map(int,input().split()))
+c=0
+for i in range(len(m)):
+    for j in range(i,len(m)):
+        if m[i]+m[j]==0:
+            print(m[i],m[j])
+            c=c+1
+            break
+    if c==1:
+        break
