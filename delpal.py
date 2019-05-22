b53136c1c77fec0ef6bb34bb5fa76bee7bcf6bff
gurudurairaj <46440938+gurudurairaj@users.noreply.github.com>
2019-05-22 13:25:09 +0530

    Create delpal.py

---
+++ delpal.py
@@ -0,0 +1,13 @@
+a=input()
+s=""
+c=0
+for i in range(len(a)):
+    s=a[0:i]+a[i+1:len(a)]
+    if s==s[::-1]:
+        c=c+1
+        break
+    s=""
+if c==1:
+    print("YES")
+else:
+    print("NO")
